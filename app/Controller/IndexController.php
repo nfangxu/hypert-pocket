a19a94fac49b5c89ba7a97c4c71ad09456051e57
nfangxu <nfangxu@gmail.com>
2019-11-06 17:45:52 +0800

    redirect to /note/index

--- app/Controller/IndexController.php
+++ app/Controller/IndexController.php
@@ -13,14 +13,22 @@ declare(strict_types=1);
 namespace App\Controller;
 
 use Hyperf\HttpServer\Annotation\AutoController;
+use Hyperf\HttpServer\Contract\ResponseInterface;
+use Psr\Http\Message\ResponseInterface as Psr7ResponseInterface;
 
 /**
  * @AutoController
  */
 class IndexController extends AbstractController
 {
-    public function index()
+    public function index(ResponseInterface $response)
     {
-        return "Default-Index";
+        return $this->redirect($response);
+    }
+
+    public function redirect(ResponseInterface $response): Psr7ResponseInterface
+    {
+        // redirect() 方法返回的是一个 Psr\Http\Message\ResponseInterface 对象，需再 return 回去
+        return $response->redirect('/note/index');
     }
 }
